5626cbf3e7d7366f3f8890716b8ed61a64873d92
DenisBukva <denisbukva@hotmail.com>
2021-03-12 13:41:06 +0100

    add ex14

---
+++ ex14/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+    //Only change code below this code
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var worldBlanks = + myNoun + " " + myAdjective + " " + myVerb + " " + myAdverb + ".";
+
+    
+
+    return worldBlanks;
+}
+
+console.log(main());
+
+module.exports = main;
