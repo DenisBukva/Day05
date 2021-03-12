3dc34913642c3d6e99438d73df44b1a82f5934ec
DenisBukva <denisbukva@hotmail.com>
2021-03-12 13:10:00 +0100

    add ex13

---
+++ ex13/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    //Only change below this line
+    var lastName = "Bezos"
+    var thirdToLastLetterOfLastName = lastName[lastName.length - 3]
+    
+    //Only change below this line
+
+    return thirdToLastLetterOfLastName;
+}
+
+console.log(main());
+
+module.exports = main;
