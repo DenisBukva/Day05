5db942ad16be16ecd4a045053087ef573b741298
DenisBukva <denisbukva@hotmail.com>
2021-03-12 12:11:37 +0100

    add ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    //Only change code below this line
+    var myString = "Jello World";
+    myString[0] = "H";
+    var myString = "Jello World";
+    myString = "Hello World"
+
+    return myString;
+}
+
+console.log(main());
+
+module.exports = main;
