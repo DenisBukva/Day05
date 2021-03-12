3e0ae5918fdf4ec3e48cc2430383a22ee7f1a553
DenisBukva <denisbukva@hotmail.com>
2021-03-12 12:03:34 +0100

    add ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    //Only change below this line
+    var lastName = "Doe";
+    var firstLetterOfLastName = "D";
+    firstLetterOfLastName = lastName[0];
+    //Only change below this line
+
+    return firstLetterOfLastName;
+}
+
+console.log(main());
+
+module.exports = main;
