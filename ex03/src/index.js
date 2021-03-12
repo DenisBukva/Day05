ec0117b4efcd777eefb34e9c0db6ba28c27dc494
DenisBukva <denisbukva@hotmail.com>
2021-03-12 10:55:10 +0100

    add ex03

---
+++ ex03/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+    //Only change code below this line
+    var myString = "First\n\t\SecondLine\nThirdLine"
+    //Only change code below this line
+
+
+    return myString;
+}
+
+console.log(main());
+
+module.exports = main;
