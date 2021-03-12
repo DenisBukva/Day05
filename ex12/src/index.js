a5f334fb73d6dc7af1492f05c25df02b63d45b85
DenisBukva <denisbukva@hotmail.com>
2021-03-12 12:59:24 +0100

    add ex12

---
+++ ex12/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    //Only change below this line
+    var lastName = "Tesla"
+    var lastLetterOfLastName = lastName[lastName.length - 1]
+    
+    //Only change below this line
+
+    return lastLetterOfLastName;
+}
+
+console.log(main());
+
+module.exports = main;
