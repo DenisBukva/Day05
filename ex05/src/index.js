eb906761959de63550ddaf4636bf8cb0e28fa174
DenisBukva <denisbukva@hotmail.com>
2021-03-12 11:15:15 +0100

    add ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+    //Only change code below this line
+    var myString = "Concatenation is the process of appending ";
+
+    myString += "one string to the end of another string.";
+
+    return myString;
+}
+
+console.log(main());
+
+module.exports = main;
